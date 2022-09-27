efa7d170fcd1ec58fd23445f5727c63673beb9b4
Jisen Ren <1500017779@pku.edu.cn>
2022-09-28 00:04:29 +0200

    fix image path problem

--- js/review.js
+++ js/review.js
@@ -1,9 +1,9 @@
 function submit_review() {
-    document.getElementById('p1').checked = false;
+    /* document.getElementById('p1').checked = false;
     document.getElementById('p2').checked = false;
     document.getElementById('p3').checked = false;
     document.getElementById('p4').checked = false;
     document.getElementById('p5').checked = false;
-    document.getElementById('review-txt').innerHTML = '';
+    document.getElementById('review-txt').innerHTML = ''; */
     alert('Thanks for visiting our website! 🤗');
 }
